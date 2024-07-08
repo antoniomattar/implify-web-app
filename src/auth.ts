23c5ea65d38ea1f4657a3ecf889f1ef4737d9342
antoniomattar <antoniomattar123@icloud.com>
2024-07-08 20:15:04 +0300

    [FIX] commented authorization to allow Vercel Deployment without errors

--- src/auth.ts
+++ src/auth.ts
@@ -21,20 +21,20 @@ export const { auth, signIn, signOut } = NextAuth({
   providers: [
     Credentials({
       async authorize(credentials) {
-        const parsedCredentials = z
-          .object({ email: z.string().email(), password: z.string().min(6) })
-          .safeParse(credentials);
+        // const parsedCredentials = z
+        //   .object({ email: z.string().email(), password: z.string().min(6) })
+        //   .safeParse(credentials);
 
-        if (parsedCredentials.success) {
-          const { email, password } = parsedCredentials.data;
-          const user = await getUser(email);
-          if (!user) return null;
+        // if (parsedCredentials.success) {
+        //   const { email, password } = parsedCredentials.data;
+        //   const user = await getUser(email);
+        //   if (!user) return null;
 
-          const passwordsMatch = await bcrypt.compare(password, user.password);
-          if (passwordsMatch) return user;
-        }
+        //   const passwordsMatch = await bcrypt.compare(password, user.password);
+        //   if (passwordsMatch) return user;
+        // }
 
-        console.log('Invalid credentials');
+        // console.log('Invalid credentials');
         return null;
       },
     }),
